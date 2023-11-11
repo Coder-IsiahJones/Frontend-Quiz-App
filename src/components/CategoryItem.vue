33e0e10cd92dfcb9abac985623a72eafc938ff29
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-11-11 16:25:52 -0500

    Create the home page

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { name } = defineProps(['name']);
+</script>
+
+<template>
+  <button
+    type="button"
+    class="flex w-[564px] items-center gap-8 rounded-3xl bg-brandWhite p-6 drop-shadow-sm hover:opacity-90 hover:shadow-md focus-visible:outline-4 focus-visible:outline-brandPurple"
+  >
+    <slot></slot>
+
+    <span class="font-rubik text-2xl font-medium text-brandDarkNavy">{{ name }}</span>
+  </button>
+</template>
---
+++ src/components/WelcomeText.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Welcome Text -->
+  <div class="flex flex-col">
+    <h1 class="font-rubik text-6xl font-light text-brandDarkNavy">Welcome to the</h1>
+    <span class="text-6xl font-medium text-brandDarkNavy">Frontend Quiz!</span>
+
+    <p class="mt-12 font-rubik text-xl font-normal italic text-brandGray">
+      Pick a subject to get started.
+    </p>
+  </div>
+</template>
