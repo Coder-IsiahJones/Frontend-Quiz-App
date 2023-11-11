c782dbbd3c4f38e25b3ace3560c61518ddf51b42
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-11-11 14:31:59 -0500

    Add the background image with icons

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BackgroundPattern from '@/components/icons/BackgroundPattern.vue';
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline text-red-600">Hello world!</h1>
+  <main class="bg-brandLightBlue h-screen">
+    <BackgroundPattern class="absolute h-screen w-screen" :darkMode="false" />
+
+    <h1 class="text-3xl font-bold underline text-red-600">Hello world!</h1>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/icons/BackgroundPattern.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+
+// Define a prop to determine whether to show the dark or light SVG
+const props = defineProps<{
+  darkMode: PropType<boolean>;
+}>();
+</script>
+
+<template>
+  <!-- Use a ternary operator to conditionally render the SVG based on the prop value -->
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1440"
+    height="960"
+    fill="none"
+    viewBox="0 0 1440 960"
+  >
+    <g :stroke="props.darkMode ? '#2D3949' : '#edf1f9'" stroke-width="144">
+      <circle cx="-50.5" cy="75.5" r="416.5" />
+      <circle cx="1388.5" cy="840.5" r="416.5" />
+    </g>
+  </svg>
+</template>
