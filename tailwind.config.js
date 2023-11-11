af147b19d0b529c670f5f2f50cdf018f131a02b5
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-11-11 13:56:59 -0500

    Add colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        // Add your custom colors here
+        brandPurple: '#A729F5',
+        brandDarkNavy: '#313E51',
+        brandNavy: '#3B4D66',
+        brandGray: '#626C7F',
+        brandBluish: '#ABC1E1',
+        brandLightBlue: '#F4F6FA',
+        brandWhite: '#FFFFFF',
+        brandGreen: '#26D782',
+        brandRed: '#EE5454'
+      }
+    }
   },
   plugins: []
 }
