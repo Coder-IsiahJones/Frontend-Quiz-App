bdadb46bb6d24d0297344db6c1c0574d1025cfc6
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-11-11 17:17:27 -0500

    Add the toggle to the page

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,17 @@
 <script setup lang="ts">
 import BackgroundPattern from '@/components/icons/BackgroundPattern.vue';
-import CategoryItem from '@/components/CategoryItem.vue';
-import HtmlIcon from '@/components/icons/HtmlIcon.vue';
-import CssIcon from '@/components/icons/CssIcon.vue';
-import JavascriptIcon from '@/components/icons/JavascriptIcon.vue';
-import AccessibilityIcon from '@/components/icons/AccessibilityIcon.vue';
-import WelcomeText from '@/components/WelcomeText.vue';
-
-const items = [
-  { icon: HtmlIcon, name: 'HTML', background: '#FFF1E9' },
-  { icon: CssIcon, name: 'CSS', background: '#E0FDEF' },
-  { icon: JavascriptIcon, name: 'Javascript', background: '#EBF0FF' },
-  { icon: AccessibilityIcon, name: 'Accessibility', background: '#F6E7FF' },
-];
+import HomePage from './components/HomePage.vue';
+import Switch from '@/components/Switch.vue';
 </script>
 
 <template>
   <main class="h-screen bg-brandLightBlue">
     <BackgroundPattern class="absolute h-screen w-screen" :darkMode="false" />
 
-    <section class="isolate flex justify-between px-[140px] py-[260px]">
-      <WelcomeText />
+    <section class="isolate px-36 py-32">
+      <Switch class="flex justify-end pb-12" />
 
-      <!-- List of Categories -->
-      <div class="flex flex-col gap-6">
-        <CategoryItem v-for="item in items" :key="item.name" :name="item.name">
-          <component
-            class="h-[56px] w-[56px] rounded-md p-2"
-            :style="{ backgroundColor: item.background }"
-            :is="item.icon"
-          />
-        </CategoryItem>
-      </div>
+      <HomePage />
     </section>
   </main>
 </template>
---
+++ src/components/Switch.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts" setup>
+import SunIcon from './icons/SunIcon.vue';
+import MoonIcon from './icons/MoonIcon.vue';
+</script>
+
+<template>
+  <div class="flex items-center">
+    <SunIcon class="mr-5 text-xs" />
+
+    <label for="toggle" class="flex cursor-pointer items-center">
+      <input type="checkbox" id="toggle" class="peer sr-only" />
+      <div
+        class="relative block h-9 w-16 rounded-full bg-brandPurple p-1 before:absolute before:left-1 before:h-7 before:w-7 before:rounded-full before:bg-brandWhite before:p-1 before:transition-all before:duration-500 peer-checked:before:left-8 peer-checked:before:bg-white"
+      ></div>
+    </label>
+
+    <MoonIcon class="ml-5 text-xs" />
+  </div>
+</template>
